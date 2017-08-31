22376a6465a351ac2371f2dea2dd57b42df127f0
Ed Gibbs <edward_gibbs@yahoo.com>
2017-08-30 21:50:45 -0500

    Testing search filter logic

--- js/__tests__/search.spec.jsx
+++ js/__tests__/search.spec.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import {shallow} from 'enzyme';
+import preload from '../../data.json';
 import Search from '../Search';
+import ShowCard from '../ShowCard';
 
 describe('Search', () => {
   it('renders correctly', () => {
     const component = shallow(<Search />);
     expect(component).toMatchSnapshot();
+  });
+
+  describe('given no search terms', () => {
+    it('returns everything', () => {
+      const component = shallow(<Search />);
+      expect(component.find(ShowCard).length).toEqual(preload.shows.length);
+    });
   });
 });
