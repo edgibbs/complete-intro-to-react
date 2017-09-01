7432041a88bbd3a973a9e7382edb01e016669d68
Ed Gibbs <edward_gibbs@yahoo.com>
2017-08-31 22:29:43 -0700

    Adding hot load

--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 const Landing = () => (
   <div className="landing">
-    <h1>svideo</h1>
+    <h1>something to search</h1>
     <input type="text" placeholder="Search" />
     <Link to="/search">or Browse All</Link>
   </div>
