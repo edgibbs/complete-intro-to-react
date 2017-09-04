50b44a38e1643f3dbf763cea94b7ad546229f9a7
Ed Gibbs <edward_gibbs@yahoo.com>
2017-09-04 13:36:03 -0700

    Utilize axios for ajax calls (very testable?)

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,6 +1,6 @@
 // @flow
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ShowCard from './ShowCard';
 import Header from './Header';
 
@@ -11,18 +11,26 @@ class Search extends Component {
   props: {
     shows: Array<Show>
   };
-  handleSearchTermChange = (event: SyntheticKeyboardEvent & { target: HTMLInputElement }) => {
-    this.setState({ searchTerm: event.target.value });
+  handleSearchTermChange = (
+    event: SyntheticKeyboardEvent & {target: HTMLInputElement}
+  ) => {
+    this.setState({searchTerm: event.target.value});
   };
   render() {
     return (
       <div className="search">
-        <Header searchTerm={this.state.searchTerm} showSearch handleSearchTermChange={this.handleSearchTermChange} />
+        <Header
+          searchTerm={this.state.searchTerm}
+          showSearch
+          handleSearchTermChange={this.handleSearchTermChange}
+        />
         <div>
           {this.props.shows
             .filter(
               show =>
-                `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0
+                `${show.title} ${show.description}`
+                  .toUpperCase()
+                  .indexOf(this.state.searchTerm.toUpperCase()) >= 0
             )
             .map(show => <ShowCard key={show.imdbID} {...show} />)}
         </div>
