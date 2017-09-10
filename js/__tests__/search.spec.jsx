48fa809898bba7306823d5d85a080a6f270447b7
Ed Gibbs <edward_gibbs@yahoo.com>
2017-09-09 19:54:45 -0700

    Passing tests

--- js/__tests__/search.spec.jsx
+++ js/__tests__/search.spec.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import preload from '../../data.json';
-import Search from '../Search';
+import Search, {Unwrapped as UnwrappedSearch} from '../Search';
 import ShowCard from '../ShowCard';
 
 describe('Search', () => {
   it('renders correctly', () => {
-    const component = shallow(<Search shows={preload.shows} />);
+    const component = shallow(
+      <UnwrappedSearch shows={preload.shows} searchTerm="" />
+    );
     expect(component).toMatchSnapshot();
   });
 
   describe('given no search terms', () => {
     it('returns everything', () => {
-      const component = shallow(<Search shows={preload.shows} />);
+      const component = shallow(
+        <UnwrappedSearch shows={preload.shows} searchTerm="" />
+      );
       expect(component.find(ShowCard).length).toEqual(preload.shows.length);
     });
   });
@@ -20,8 +24,10 @@ describe('Search', () => {
   describe('given a search term', () => {
     it('returns only matching records', () => {
       const searchWord = 'black';
-      const component = shallow(<Search shows={preload.shows} />);
-      component.find('input').simulate('change', {target: {value: searchWord}});
+      const component = shallow(
+        <UnwrappedSearch shows={preload.shows} searchTerm={searchWord} />
+      );
+      // component.find('input').simulate('change', {target: {value: searchWord}});
       expect(component.find(ShowCard).length).toEqual(2);
     });
   });
